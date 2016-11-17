b15ff65e9f1c0cfc2ca939d541ef508c5d1d164d
Luke Westerfield <email": null>
2016-11-17 17:14:03 -0600

    merged

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
 
   # Search method for resources (posts)
   def self.search(term)
+    results = []
     if term
-      where('title LIKE ?', "%#{term}%").order('id DESC')
+      results << where('lower(title) LIKE ?', "%#{term.downcase}%").order('id DESC')
+      results << self.tagged_with([term], wild: true,:any => true)
     else
-      order('id DESC')
+      results << order('id DESC')
     end
+    # Since we're getting multi-dimensional arrays
+    results.flatten
   end
 
 
