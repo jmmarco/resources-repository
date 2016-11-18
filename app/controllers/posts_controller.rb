b70e75199fc85111c6dab0f91b1e28c5c1b91f1f
Juan Rojas <jmr5150@gmail.com>
2016-11-18 17:04:44 -0600

    Complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,8 +13,6 @@ class PostsController < ApplicationController
     end
   end
 
-<<<<<<< HEAD
-=======
   def parse
     doc = Nokogiri::HTML(open(params[:data]))
     image = doc.search('meta[property="og:image"]').pluck('content')
@@ -25,6 +23,5 @@ class PostsController < ApplicationController
       format.json { render json: data }
     end
   end
->>>>>>> master
 
 end
