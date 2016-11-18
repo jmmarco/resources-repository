ada34cdb755c27d807ffd558a0d0c83f5b98c2d3
Luke Westerfield <email": null>
2016-11-18 15:23:43 -0600

    user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def show
-    # begin
-    #   @user = User.find(params[:id])
-    # rescue
-    #   render "error.html.erb"
-    # end
+    begin
+      @user = User.find(params[:id])
+    rescue
+      render "error.html.erb"
+    end
   end
 end
